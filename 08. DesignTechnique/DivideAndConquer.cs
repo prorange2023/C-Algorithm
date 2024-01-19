c49d66f684a59f6c5b4e23b1334d260a40b21202
prorange2023 <prorange2023@gmail.com>
2024-01-19 13:38:19 +0900

    feat : 알고리즘 설계방법 추가

---
+++ 08. DesignTechnique/DivideAndConquer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique
+{
+	internal class DivideAndConquer
+	{
+
+		/*************************************************************************
+         * 분할정복 (Divide and Conquer)
+         * 
+         * 큰 문제를 작은 문제로 나눠서 푸는 하향식 접근 방식
+         * 분할을 통해서 해결하기 쉬운 작은 문제로 나눈 후 정복한 후 병합하는 과정
+         **************************************************************************/
+		/* 큰 문제를 풀어낼 때에 너무 커서 감이 안오면 작은문제로 쪼개면 된다
+		 * 문제를 쪼개다보면 해결가능한 쉬운문제가 될 것이다.
+		 * 이걸 해결하고나면 이걸 다시 모아서 큰 문제를 해결 가능할 것이다
+		 * 그래서 분할 정복
+		 */
+
+
+		// 예시 - 거듭 제곱
+
+		// 2^ 16 -> 4^8 -> 8^4-> 16^4 -> 256^2
+		// 문제를 해결하기 쉽게 만든다. 이것이 분할 정복
+
+
+		int Pow(int x, int n) // 분할정복, 이렇게하면 시간복잡도가 O(log n) 2^ 16승이면 연산횟수 9 이런식으로 줄어듬
+		{
+			if (n == 1)
+			{
+				return x;
+			}
+
+			if (n % 2 == 0)
+			{
+				return Pow(x * x, n / 2);
+			}
+			else
+			{
+				return Pow(x * x, n / 2) * x;
+			}
+		}
+
+		int Power(int x, int n)  // 이렇게 하면 시간복잡도가 O(n) 2 ^ 16승이면 연산횟수 15
+		{
+			int result = 1;
+			for (int i = 0; i < n; i++)
+			{
+				result *= x;
+
+			}
+			return result;
+		}
+
+		int Power2(int x, int n)  // 동적계획법
+		{
+			
+		}
+	}
+}
---
+++ 08. DesignTechnique/DynamicProgramming.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique
+{
+	internal class DynamicProgramming
+	{
+		/**********************************************************************************
+         * 동적계획법 (Dynamic Programming)
+         * 
+         * 작은문제의 해답을 큰문제의 해답의 부분으로 이용하는 상향식 접근 방식
+         * 주어진 문제를 해결하기 위해 부분 문제에 대한 답을 계속적으로 활용해 나가는 기법
+         * 작은 문제의 해법을 기록(메모라이징)해두는 특징이 있음
+         * 필연적으로 저장소가 필요함.
+         * 메모이제이션 이라고도 함
+         ***********************************************************************************/
+		/*
+		 * 분할정복은 큰 문제를 작은문제로 쪼개는 하향식, 이건 상향식. 
+		 * 정의를 큰문제에 맞추면 분할정복, 작은문제에 맞추면 동적계획법
+		 * 어떤 알고리즘 설계 기법이 효율적인가? 는 당연히 문제마다 다르다.
+		 */
+
+
+		// 예시 - 피보나치 수열
+		int Fibonachi(int x)  // 동적계획법 O(n)
+		{
+			int[] fibonachi = new int[x + 1];
+			fibonachi[1] = 1;
+			fibonachi[2] = 1;
+
+			// 전거와 전전꺼를 알아야 이번값을 구함. 전형적인 동적계획법
+			// 작은 문제의 해답을 알면 큰문제를 풀떄 이용해서 푼다
+
+			for (int i = 3; i <= x; i++)
+			{
+				fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+			}
+
+			return fibonachi[x];
+		}
+
+		int fibonachi(int x) // 분할정복 O(n^2) 
+		{
+			if (x == 1)
+				return 1;
+			else if (x == 2)
+				return 1;
+
+			return fibonachi(x - 1) + fibonachi(x - 2);
+		}
+
+	}
+}
