cb0827984e73bc7942d2fb7c62afc9200fb5fc35
prorange2023 <prorange2023@gmail.com>
2024-01-24 09:07:43 +0900

    feat : 11. 다익스트라 알고리즘 추가

---
+++ 11. ShortesPath/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _11._ShortesPath
+{
+	internal class Program
+	{
+		const int INF = 99999;
+
+		static void Main(string[] args)
+		{
+			int[,] graph = new int[9, 9]
+			{
+				{   0, INF,   1,   7, INF, INF, INF,   5, INF},
+				{ INF,   0, INF, INF, INF,   4, INF, INF, INF},
+				{ INF, INF,   0, INF, INF, INF, INF, INF, INF},
+				{   5, INF, INF,   0, INF, INF, INF, INF, INF},
+				{ INF, INF,   9, INF,   0, INF, INF, INF,   2},
+				{   1, INF, INF, INF, INF,   0, INF,   6, INF},
+				{ INF, INF, INF, INF, INF, INF,   0, INF, INF},
+				{   1, INF, INF, INF,   4, INF, INF,   0, INF},
+				{ INF,   5, INF,   2, INF, INF, INF, INF,   0}
+			};
+
+			Dijkstra.ShortestPath(in graph, 0, out int[] distance, out int[] path);
+
+			Console.WriteLine("<Dijkstra>");
+			PrintDijkstra(distance, path);
+		}
+
+		private static void PrintDijkstra(int[] distance, int[] path)
+		{
+			Console.WriteLine($"{"Vertex",8}{"Visit",8}{"Path",8}");
+
+			for (int i = 0; i < distance.Length; i++)
+			{
+				Console.Write($"{i,8}");
+
+				if (distance[i] >= INF)
+				{
+					Console.Write($"{"INF",8}");
+				}
+				else
+				{
+					Console.Write($"{distance[i],8}");
+				}
+
+				Console.WriteLine($"{path[i],8}");
+			}
+		}
+	}
+}
